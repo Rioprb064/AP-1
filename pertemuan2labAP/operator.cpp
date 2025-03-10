e936a498ab5029fe6649761c39c749b2e7ecac7a
Rioprb064 <rionaldopurba2006@gmail.com>
2025-03-10 09:20:13 +0700

    Pertemuan 2 : Penggunaan Operator

---
+++ pertemuan2labAP/operator.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    system("cls");
+
+    //4 assignment operator (=)
+    a = 3;
+    b = 5;
+
+    //arithmetical operator (+, -, *, /, %)
+
+    //int tambah = a + b;
+    //int kurang = a - b;
+    //int kali = a * b;
+    //float bagi = (float)a / (float)b; //type casting
+    //int modulo = a % b;
+
+    //cout<<"Hasil penjumlahan : "<<tambah<<endl;
+    //cout<<"Hasil pengurangan : "<<kurang<<endl;
+    //cout<<"Hasil perkalian : "<<kali<<endl;
+    //cout<<"Hasil pembagian : "<<bagi<<endl;
+    //cout<<"Hasil sisa bagi : "<<modulo<<endl;
+
+    //relational operator
+    //cout<<(a==b)<<endl;
+    //cout<<(a>b)<<endl;
+    //cout<<(a>=b)<<endl;
+    //cout<<(a<b)<<endl;
+    //cout<<(a<=b)<<endl;
+    //cout<<(a!=b)<<endl;
+
+    //logical operator (&&, ||, !)
+    //cout << (true && true) << endl;
+    //cout << (true && false) << endl;
+    //cout << (false && true) << endl;
+    //cout << (false && false) << endl;
+
+    //cout << (true || true) << endl;
+    //cout << (true || false) << endl;
+    //cout << (false || true) << endl;
+    //cout << (false || false) << endl;
+
+    //cout<<!true<<endl;
+    //cout<<!false<<endl;
+
+    //bitwise operator (&, |, ~, ^, <<, >>) --> operasi biner
+    //cout << (5 & 7) << endl;
+    //cout << (5 | 7) << endl;
+    //cout << (5 ^ 7) << endl;
+    //cout << (~7) << endl;
+    //cout << (7 << 2) << endl;
+    //cout << (7 >> 2) << endl;
+
+    //shorthand
+    //a += 7 --> a = a + 7
+    //a+=7; //a = a + 7
+    //cout<<a<<endl;
+
+    //a-=7; //a = a - 7
+    //cout<<a<<endl;
+
+    //a*=7; //a = a * 7
+    //cout<<a<<endl;
+
+    //a/=7; //a = a / 7
+    //cout<<a<<endl;
+
+    //increment & decrement
+    //pre increment
+    //cout<<++a<<endl;
+    //cout<<b<<endl;
+    //cout<<++b<<endl;
+    //cout<<a<<endl;
+
+    //post increment
+    //cout<<a<<endl;
+    //cout<<a++<<endl;
+    //cout<<a<<endl;
+    //cout<<b<<endl;
+    //cout<<b++<<endl;
+    //cout<<b<<endl;
+
+    //pre decrement
+    //cout<<--a<<endl;
+    //cout<<a<<endl;
+    //cout<<--b<<endl;
+    //cout<<b<<endl;
+
+    //post decrement
+    //cout<<a<<endl;
+    //cout<<a--<<endl;
+    //cout<<a<<endl;
+    //cout<<b<<endl;
+    //cout<<b--<<endl;
+    //cout<<b<<endl;
+}
